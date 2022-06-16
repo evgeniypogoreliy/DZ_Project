164539033296f1a4fe697d9997b0a83f4b68729d
Gazim-Kerimov <gazim94@mail.ru>
2022-06-16 13:00:13 +0300

    Добавлен функционал модуля process_file. Методы принимают котреж или список, а затем записывают данные в два отдельных файла. Данные записываются в следующем формате: Завершенная/планируемая задача № __: *данные из списка/кортежа*. Каждая новая запись добавляется в файл с новой строки

--- process_file.py
+++ process_file.py
@@ -1,15 +1,31 @@
-
+completed_count = 1
+plan_count = 1
 
 def writer_completed(list):
-    file = open('Completed_task.csv' 'w')
-    file.write(str(list))
-    file.close()
+    global completed_count
+    with open('completed_task.csv', 'a') as file:
+        file.write(f'Завершенная задача №{completed_count}: ')
+        for e in list:
+            file.write(f'{e} ')
+        file.write('\n')    
+    completed_count +=1        
+    
 
 def writer_planned(list):
-    file = open('planned_task.csv' 'w')
-    file.write(str(list))
-    file.close()
+    global plan_count
+    with open('planned_task.csv', 'a') as file:
+        file.write(f'Планируемая задача №{plan_count}: ')
+        for e in list:
+            file.write(f'{e} ')
+        file.write('\n')    
+    plan_count +=1     
 
+a = ('25.07', 'Завтрак', 'мюсли')
+b = ('26.07', 'Встреча', 'По работе')
 
+writer_completed(a)
+writer_completed(b)
 
+writer_planned(a)
+writer_planned(b)
     
