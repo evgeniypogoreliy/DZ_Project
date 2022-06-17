8af6720341688289965013d37349818b2c7f5e8c
Irina5011 <100015660+Irina5011@users.noreply.github.com>
2022-06-17 14:43:22 +0700

    Update find_data.py
    Completed

--- find_data.py
+++ find_data.py
@@ -1,5 +1,26 @@
+#planned_task.csv
+#completed_task.csv
 
+def find_plans(filename, key_word):
 
-def find():
-    res = ""
-    return res
+    find_plans = open(filename, mode='r+')
+
+    find_plans = find_plans.readlines()
+
+    for line in find_plans:
+
+        if key_word.lower() in line.lower():
+
+            print(line)
+
+start = int(input("Привет! Какой блокнот тебе открыть? Если 'Планы', то введи цифру 1, если 'Завершенные дела', то введи цифру 2 =>"))
+
+if start == 1:
+    key_word = input("Введи ключевое слово для поиска задачи => ")
+    find_plans('planned_task.csv', key_word)
+elif start ==2:
+    key_word = input("Введи ключевое слово для поиска задачи => ")
+    find_plans('completed_task.csv', key_word)
+else:
+    print("Товарищ, такого блокнота у тебя нет! Только 1 или 2.")
+
