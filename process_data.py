a770a8a43eed81a12aac31fe2c5f75469230aff3
Andrey <y899yy001>
2022-06-17 19:38:21 +0300

    Ввод данных от пользователя

--- process_data.py
+++ process_data.py
@@ -19,16 +19,20 @@
 
 print('Привет user')
 
-user_input = input('Введите  запись (в качестве разделителя используйте пробел): ')
-key_note = int(input("В какой блолкнот внести запись? Если 'Планы', то введите цифру 1, если 'Завершённые дела', то введите цифру 2 =>  "))
+user_input = input(
+    'Введите  запись (в качестве разделителя используйте пробел): ')
+key_note = int(input(
+    "В какой блолкнот внести запись? Если 'Планы', то введите цифру 1, если 'Завершённые дела', то введите цифру 2 =>  "))
+
 
 def creating_record(string):
     if key_note == 1:
-        plan_record = tuple( i for i in string.split(' '))
+        plan_record = tuple(i for i in string.split(' '))
         return plan_record
     elif key_note == 2:
-        compl_record = tuple( i for i in string.split(' '))
-        return compl_record 
-    else: print('Неверно задан номер блокнота! Введите цифру 1 Планирование дела или цифру 2 для Завершенного дела')       
+        compl_record = tuple(i for i in string.split(' '))
+        return compl_record
+    else:
+        print('Неверно задан номер блокнота! Введите цифру 1 Планирование дела или цифру 2 для Завершенного дела')
 
-#print(creating_record(user_input))
+# print(creating_record(user_input))
